146a4c1f62775057ed5be38621884bc3ecbd4346
Anil Tolwani <anil@unlearn.ai>
2024-10-06 01:27:10 -0700

    all

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { EnergyConsumptionVisualizationComponent } from "@/components/latency-visualization";
+import { EnergyConsumptionVisualizationComponent } from "@/components/energy-consumption-visualization";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24">
-      <h1 className="text-lg font-bold mb-6 font-mono">GPU Napkin Math</h1>
+      <h1 className="text-lg font-bold mb-6 font-mono">Energy Consumption Visualization</h1>
       <EnergyConsumptionVisualizationComponent />
     </main> 
   );
